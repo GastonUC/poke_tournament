0be02b6741f12007e8653e3ed9998a429585cde6
Gaston Umaña <Gacox.magallanes@gmail.com>
2022-07-16 10:04:00 -0400

    Se agrega Gemfile

--- tournament.rb
+++ tournament.rb
@@ -4,6 +4,7 @@
 
 # Se utilzará un formato estándar de campeonato, serán 4 batallas por lado A y 4 batallas en el lado B.
 
+# Form N3
 # hash = {"charmander" => {"hp":56, "att":61, "def":30, "satt":58, "sdef":54, "spd":34, "type":"fire"}, "bulbasaur" => {"hp":56, "att":80, "def":48, "satt":60, "sdef":70, "spd":43, "type":"water"}}
 # puts hash["charmander"][:hp]
 
@@ -28,6 +29,7 @@ end
 
 
 # Obtiene los links a las paginas correspondientes a los pokemon y los envia al metodo para obtener los base_stats y el tipo
+# Migrar metodo a get_pokemons
 def get_base_data(poke_lim)
     pok_link = []
     pok_data = []
@@ -50,46 +52,39 @@ end
 def getting_stats_type(key, link)
     poke = []
     stats = []
+    type = []
     urls = HTTParty.get(link)["stats"]
     link2 = HTTParty.get(link)["types"]
     for x in link2
-        type1 = x["type"]["name"]
+        type = x["type"]["name"]
     end
     for i in urls
         stat_name = i["stat"]["name"]
         stat = i["base_stat"]
         stats << [stat_name, stat]
     end
-    stats << ["type",type1]
+    for y in type
+        stats << ["type",type]
+    end
     poke << [key, stats.to_h]
     return poke.to_h
 end
 
 puts get_base_data(pokemon_limit)
 
-# link = HTTParty.get("https://pokeapi.co/api/v2/pokemon/charmander/")["types"]
-# for x in link
-#     puts x["type"]["name"]
+
+def poke_tournament()
+    puts "hi"
+end
+
+# poke_tournament()
+
+# stat = HTTParty.get("https://pokeapi.co/api/v2/pokemon/charizard/")["types"]
+# type = []
+# for x in stat
+#     type << x["type"]["name"]
 # end
-
-# Para obtener los base_stat
-# response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/90/")['stats']
-# puts response[0]['base_stat']
-
-
-# puts response["stats"]
-# for r in response
-#     puts r[1]
-# end
-
-# puts response['stats']
-
-
-
-# type = get_pokemon_type(contestants, uri)
 # puts type
-# puts get_pokemon_type(uri)
-
 
 # class Pokemons
 #     include HTTParty
